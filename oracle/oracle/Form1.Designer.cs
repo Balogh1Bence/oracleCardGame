16ae5f04bd8cd4f6830e21ec668c50ea62219686
Balogh1Bence <45355550+Balogh1Bence@users.noreply.github.com>
2019-11-28 21:04:39 +0100

    Add files via upload
    started cardgame

---
+++ oracle/oracle/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace oracle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(164, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(216, 319);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 202);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ oracle/oracle/card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AlphaUtils;
+using System.Windows.Forms;
+namespace oracle
+{
+    public class Card 
+    {
+        private GroupBox cardHolder;
+        private PictureBox cardImage;       
+        private AlphaUtils.AlphaRichTextBox cardHealth;
+        private AlphaRichTextBox cardAttack;
+        private AlphaRichTextBox cardDef;
+        private AlphaRichTextBox cardPrice;
+        private AlphaRichTextBox cardText;
+        public string imageLink { get; set; }
+        public int defense { get; set; }
+        public string name { get; set; }
+        public int ID { get; set; }
+        public int price { get; set; }
+        public int health { get; set; }
+        public int attack { get; set; }
+        public int specID { get; set; }
+        public string god { get; set; }
+        public int type { get; set; }
+        public int prod { get; set; }
+        public Card(string[] args) {
+
+            cardHolder = new GroupBox();
+            cardImage = new PictureBox();
+
+            cardHealth = new AlphaRichTextBox();
+            cardAttack = new AlphaRichTextBox();
+            cardDef = new AlphaRichTextBox();
+            cardPrice = new AlphaRichTextBox();
+            cardText = new AlphaRichTextBox();
+
+            cardHolder.Controls.Add(cardImage);
+            cardHolder.Controls.Add(cardHealth);
+        }
+        
+    }
+}
